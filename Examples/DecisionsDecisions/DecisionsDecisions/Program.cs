2cb6dbd78984f69fc1948a999a4fb6d33e77dc2a
rosstimo <rosstimo@isu.edu>
2025-09-08 08:27:21 -0600

    add if/else and if/else if/else

--- Examples/DecisionsDecisions/DecisionsDecisions/Program.cs
+++ Examples/DecisionsDecisions/DecisionsDecisions/Program.cs
@@ -15,6 +15,35 @@
                 Console.WriteLine("my number is 5");
             }
 
+            if (theNumber < 6) 
+            {
+                //runs when true
+                Console.WriteLine("The number is less than 6");
+            }
+            else
+            {
+                // runs when false
+                Console.WriteLine("The number is bigger than 6");
+            }
+
+            theNumber = 7;
+
+            if (theNumber < 6)
+            {
+                Console.WriteLine("The number is less than 6");
+            }
+            else if (theNumber == 6)
+            {
+                Console.WriteLine("The number is 6");
+            }
+            else
+            {
+                Console.WriteLine("The number is greater than 6");
+            }
+
+
+
+
                 //pause
                 Console.Read();
         }
