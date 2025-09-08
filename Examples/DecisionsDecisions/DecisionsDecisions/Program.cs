b34ee499847945f36ab2281b924db341c320a5cd
rosstimo <rosstimo@isu.edu>
2025-09-08 08:14:31 -0600

    add comments

--- Examples/DecisionsDecisions/DecisionsDecisions/Program.cs
+++ Examples/DecisionsDecisions/DecisionsDecisions/Program.cs
@@ -7,8 +7,11 @@
 
             int theNumber = 5;
 
+            //simple if statement
             if (theNumber == 5)
             {
+                //when the evaluation is true all code here runs
+                //when false all code here is skipped
                 Console.WriteLine("my number is 5");
             }
 
