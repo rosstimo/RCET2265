70ad5abd69e645cacd219d1c272f7e6d3f6df48d
rosstimo <rosstimo@isu.edu>
2025-09-09 08:34:19 -0600

    add for loop examples

--- Examples/LooptyLoops/LooptyLoops/Program.cs
+++ Examples/LooptyLoops/LooptyLoops/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //for loop. loops a number of times
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            //nested for loop
+
+            for (int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            //pause
+            Console.Read();
         }
     }
 }
