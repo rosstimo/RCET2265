c80f4e9806e9573d494dc0f7816a23d3c8d523d7
rosstimo <rosstimo@isu.edu>
2025-09-11 08:24:35 -0600

    good start

--- Examples/ControlFlowExample/ControlFlowExample/Program.cs
+++ Examples/ControlFlowExample/ControlFlowExample/Program.cs
@@ -4,8 +4,21 @@
     {
         static void Main(string[] args)
         {
+            int firstNumber = 0;
+            string userInput;
+
+
+            //ask for whole number
+            //user must enter a valid whole number or quit
+
+            Console.WriteLine("Please Enter a Whole Number");
+            userInput = Console.ReadLine();
+            Console.WriteLine($"You Entered: {userInput}");
+            firstNumber = int.Parse(userInput);
+
+
             //pause
-            Console.Read()
+            Console.Read();
         }
     }
 }
