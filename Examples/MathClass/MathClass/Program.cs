b829d3ada8239deee7824ec5f7683e32d3878e7c
rosstimo <rosstimo@isu.edu>
2025-09-15 08:51:42 -0600

    add random number example

--- Examples/MathClass/MathClass/Program.cs
+++ Examples/MathClass/MathClass/Program.cs
@@ -5,9 +5,19 @@
         static void Main(string[] args)
         {
             int firstNumber = 5;
+            int currentRandomNumber = 0;
+            Random randomNumber = new Random();
 
-            Console.WriteLine( Math.Sqrt(firstNumber) * Math.Sqrt(firstNumber));
-            Console.WriteLine(Math.Pow(2, 2));
+            //Console.WriteLine( Math.Sqrt(firstNumber) * Math.Sqrt(firstNumber));
+            //Console.WriteLine(Math.Pow(2, 2));
+
+            for (int i = 0; i < 100; i++)
+            {
+                currentRandomNumber = randomNumber.Next(1, 7); // Random number between 1 and 6
+                Console.WriteLine(currentRandomNumber);
+
+            }
+
 
             //pause
             Console.Read();
