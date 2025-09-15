3931ef5d76be6b8416e4ac87d8f9e6a2a383d833
rosstimo <rosstimo@isu.edu>
2025-09-15 08:30:58 -0600

    add some string class examples

---
+++ Examples/WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace WorkingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string theString = "Hello, World!";
+
+            theString = theString.ToLower();
+
+
+            Console.WriteLine(theString);
+
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                Console.Write((i + j).ToString().PadLeft(3) + " |");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"The string '{theString}' is {theString.Length} characters long.");
+
+
+
+            //pause
+            Console.Read();
+        }
+    }
+}
