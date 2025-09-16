60974a2bc2cd613e02b64f1b980d66215d076787
rosstimo <rosstimo@isu.edu>
2025-09-16 08:43:31 -0600

    iterate through an array

--- Examples/ArrayExample/ArrayExample/Program.cs
+++ Examples/ArrayExample/ArrayExample/Program.cs
@@ -4,9 +4,17 @@
     {
         static void Main(string[] args)
         {
+            //single dimensional arrays 
             int[] fruit = new int[5];
             string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+            //multi-dimensional arrays
+            int[,] grid = new int[3, 5];
+            bool[,] deck = { { false , false}, { false, true } , { true, true} };
+
+
+
+            //assign values to each element
             fruit[0] = 3;
             fruit[1] = 4;
             fruit[2] = 7;
@@ -15,8 +23,7 @@
 
             Console.WriteLine(fruit[3]);
 
-
-
+            //access elements in an array
             Console.WriteLine(weekDays[0]);
             Console.WriteLine(weekDays[1]);
             Console.WriteLine(weekDays[2]);
@@ -24,16 +31,28 @@
             Console.WriteLine(weekDays[4]);
             Console.WriteLine(weekDays[5]);
             Console.WriteLine(weekDays[6]);
+            Console.WriteLine();
 
-            /*Output:
-            Sun
-            Mon
-            Tue
-            Wed
-            Thu
-            Fri
-            Sat
-            */
+            //for (int i = 0; i < 8 ; i++) //crashes
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                Console.WriteLine(weekDays[i]);
+            }
+
+            Console.WriteLine();
+            foreach (string day in weekDays)
+            {
+                Console.WriteLine(day);
+            }
+            Console.WriteLine();
+
+            //assign multi-dimensional array elements
+            grid[0, 0] = 55;
+            grid[1, 2] = 7;
+            grid[2, 4] = 23;
+
+
+
 
             //pause
             Console.Read();
