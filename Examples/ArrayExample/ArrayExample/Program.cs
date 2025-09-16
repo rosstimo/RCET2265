3e9b371fdb0ec2c5e61e14d35ed28f01439b8fa8
rosstimo <rosstimo@isu.edu>
2025-09-16 08:20:55 -0600

    add single dimension array examples

--- Examples/ArrayExample/ArrayExample/Program.cs
+++ Examples/ArrayExample/ArrayExample/Program.cs
@@ -4,8 +4,37 @@
     {
         static void Main(string[] args)
         {
-            
-            
+            int[] fruit = new int[5];
+            string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+            fruit[0] = 3;
+            fruit[1] = 4;
+            fruit[2] = 7;
+            fruit[3] = 8;
+            fruit[4] = 133;
+
+            Console.WriteLine(fruit[3]);
+
+
+
+            Console.WriteLine(weekDays[0]);
+            Console.WriteLine(weekDays[1]);
+            Console.WriteLine(weekDays[2]);
+            Console.WriteLine(weekDays[3]);
+            Console.WriteLine(weekDays[4]);
+            Console.WriteLine(weekDays[5]);
+            Console.WriteLine(weekDays[6]);
+
+            /*Output:
+            Sun
+            Mon
+            Tue
+            Wed
+            Thu
+            Fri
+            Sat
+            */
+
             //pause
             Console.Read();
         }
