5270010d56ab2d6197ef51971cb62870b9452525
rosstimo <rosstimo@isu.edu>
2025-09-17 08:21:59 -0600

    add simple method and call

--- Examples/MethodExamples/MethodExamples/Program.cs
+++ Examples/MethodExamples/MethodExamples/Program.cs
@@ -4,9 +4,21 @@
     {
         static void Main(string[] args)
         {
+            // call SayHello Method
+            SayHello();
 
             //pause
             Console.Read();
         }
+
+        //define the SayHello Method
+        static void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+
+
+
     }
 }
