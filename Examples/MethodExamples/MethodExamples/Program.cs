4680676b5aea87071f28fd2e870700956d1946c4
rosstimo <rosstimo@isu.edu>
2025-09-17 08:46:49 -0600

    demo scope and lifetime of variables

--- Examples/MethodExamples/MethodExamples/Program.cs
+++ Examples/MethodExamples/MethodExamples/Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
+            int myLuckyNumber = 5;
+
             // call SayHello Method
-            SayHello();
+            //SayHello();
+
+            //PrintSum( 5, 5);
+
+
+            Console.WriteLine(myLuckyNumber);
+            //LuckyNumber();
+            //OtherLuckyNumber(myLuckyNumber);
+            //NotSoLuckyNumber(ref myLuckyNumber);   
+            Console.WriteLine(myLuckyNumber);
+
 
             //pause
             Console.Read();
@@ -18,7 +30,25 @@
         }
 
 
+        static void PrintSum(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+        }
 
-
+        static void LuckyNumber()
+        {
+            int myLuckyNumber = 7;
+            Console.WriteLine(myLuckyNumber);
+        }
+        static void OtherLuckyNumber(int myLuckyNumber)
+        {
+            myLuckyNumber++;
+            Console.WriteLine(myLuckyNumber);
+        }
+        static void NotSoLuckyNumber(ref int myLuckyNumber)
+        {
+            myLuckyNumber++;
+            Console.WriteLine(myLuckyNumber);
+        }
     }
 }
