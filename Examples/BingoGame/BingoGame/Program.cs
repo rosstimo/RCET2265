57e60dcf9463b76ebd477aa961268f8eabe6f8ab
rosstimo <rosstimo@isu.edu>
2025-09-25 08:54:33 -0600

    start display method

--- Examples/BingoGame/BingoGame/Program.cs
+++ Examples/BingoGame/BingoGame/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Display(); // test display
+
+            Console.Read();
+        }
+
+        static void Display()
+        {
+            string currentLocation = "XX";
+            string[] header = { "B", "I", "N", "G", "O" };
+
+            foreach (string letter in header)
+            {
+                Console.Write(letter);
+            }
+
+                ,
+            //write 50 "-" to console
+            Console.WriteLine(new string('-', 25));
+
+
+
+            // Display user interface
+
+            for (int row = 0; row < 15; row++)
+            {
+                for (int column = 0; column < 5; column++)
+                {
+                    Console.Write(currentLocation.PadLeft(3) + " |");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
