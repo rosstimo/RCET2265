f930b03001539aafb547f4c3ce56165413aefbc6
rosstimo <rosstimo@isu.edu>
2025-09-26 08:52:39 -0600

    start ball drawn tracking

--- Examples/BingoGame/BingoGame/Program.cs
+++ Examples/BingoGame/BingoGame/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace BingoGame
+﻿using System.Net.NetworkInformation;
+
+namespace BingoGame
 {
     internal class Program
     {
+        private static bool[,] ballCage = new bool[14, 4];
+
+        //get the drawn/not drawn status of a specific ball
+        static bool BallIsDrawn(int row, int column)
+        {
+            bool status = false;
+
+            try
+            {
+                status = ballCage[row, column];
+            }
+            catch (Exception)
+            {
+                //reading outside the boundary of the array will crash
+                //maybe log exception here
+            }
+            return status;
+        }
+
+        static void DrawBall()
+        {
+            //get random column
+            //get random row
+            //check if drawn already
+            //yes: draw another
+            //no: success! mark as drawn
+
+            //make on ball drawn for testing
+            ballCage[5,2] = true;
+        }
+
         static void Main(string[] args)
         {
+            DrawBall();
             DisplayGrid(); // test display
 
             Console.Read();
@@ -11,9 +45,9 @@
 
         static void DisplayGrid()
         {
-            string currentLocation = "XX"; // place holder for ball numbers
+            string currentLocation = "  "; // show spaces for not drawn, number for drawn
             string[] header = { "B", "I", "N", "G", "O" }; //column header content
-            string vDivider = " |";//column seperator
+            string vDivider = " |";//column separator
             int padding = 3;//right justify numbers
             int totalWidth = 5 * (padding + vDivider.Length); //calculate total width
             string hDivider = new string('-', totalWidth); //dynamic length horizontal divider
@@ -34,6 +68,14 @@
                 for (int column = 0; column < 5; column++)
                 {
                     //need to check each possible ball and display those already drawn
+                    if (BallIsDrawn(row, column))
+                    {
+                        currentLocation = "00"; //TODO resolve actual number
+                    }
+                    else
+                    {
+                        currentLocation = "  ";
+                    }
                     Console.Write(currentLocation.PadLeft(padding) + vDivider);
                 }
                 Console.WriteLine();
