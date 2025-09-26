e7842f672fa6796ac5ace5077cfc4955004da837
rosstimo <rosstimo@isu.edu>
2025-09-26 08:27:24 -0600

    make DisplayGrid() dynamic and add comments

--- Examples/BingoGame/BingoGame/Program.cs
+++ Examples/BingoGame/BingoGame/Program.cs
@@ -4,34 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Display(); // test display
+            DisplayGrid(); // test display
 
             Console.Read();
         }
 
-        static void Display()
+        static void DisplayGrid()
         {
-            string currentLocation = "XX";
-            string[] header = { "B", "I", "N", "G", "O" };
+            string currentLocation = "XX"; // place holder for ball numbers
+            string[] header = { "B", "I", "N", "G", "O" }; //column header content
+            string vDivider = " |";//column seperator
+            int padding = 3;//right justify numbers
+            int totalWidth = 5 * (padding + vDivider.Length); //calculate total width
+            string hDivider = new string('-', totalWidth); //dynamic length horizontal divider
 
+            //print header row
             foreach (string letter in header)
             {
-                Console.Write(letter);
+                Console.Write(letter.PadLeft(padding) + vDivider);
             }
+            Console.WriteLine();
 
-                ,
-            //write 50 "-" to console
-            Console.WriteLine(new string('-', 25));
-
-
+            //horizontal divider
+            Console.WriteLine(hDivider);
 
             // Display user interface
-
             for (int row = 0; row < 15; row++)
             {
                 for (int column = 0; column < 5; column++)
                 {
-                    Console.Write(currentLocation.PadLeft(3) + " |");
+                    //need to check each possible ball and display those already drawn
+                    Console.Write(currentLocation.PadLeft(padding) + vDivider);
                 }
                 Console.WriteLine();
             }
