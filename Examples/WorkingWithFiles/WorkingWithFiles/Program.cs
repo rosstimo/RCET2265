61f3a7dcde1e0c5d2faab4c82ca414e4bb7cf2a6
rosstimo <rosstimo@isu.edu>
2025-10-06 08:52:40 -0600

    add write, append, read examples. discussed relative file paths

--- Examples/WorkingWithFiles/WorkingWithFiles/Program.cs
+++ Examples/WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -4,6 +4,33 @@
     {
         static void Main(string[] args)
         {
+            string path = "test.txt";
+
+            // open/create a new file to write text
+            // overwrites existing content!!
+            using (StreamWriter testFile = File.CreateText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    testFile.WriteLine("this is a line of text");
+                }
+            }
+
+            // open/create a new file to append text
+            // Appends text to existing file content
+            using (StreamWriter testFile = File.AppendText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    testFile.WriteLine("Append this line of text");
+                }
+            }
+
+            using (StreamReader testFile = new StreamReader(path))
+            {
+                Console.WriteLine(testFile.ReadLine());
+            }
+           
 
             Console.Read();
         }
