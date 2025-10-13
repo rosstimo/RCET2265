c434e8158c64ae4585d86ff90c8ed23528fe6a87
rosstimo <rosstimo@isu.edu>
2025-10-13 08:48:09 -0600

    add some basic list example

--- Examples/ListExamples/ListExamples/Program.cs
+++ Examples/ListExamples/ListExamples/Program.cs
@@ -4,6 +4,45 @@
     {
         static void Main(string[] args)
         {
+            // constructor
+            List<string> names = new List<string>();
+
+            //add to the end of the list
+            names.Add("Jim");
+            names.Add("Bob");
+            names.Add("Mary");
+            names.Add("Jill");
+            names.Add("Frank");
+            names.Add("Billy");
+            names.Add("Pam");
+            names.Add("Sheila");
+            names.Add("Jerry");
+
+
+            //get the first index number for "Billy"
+            Console.WriteLine(names.IndexOf("Billy"));
+
+            // insert item to an exact index in the middle
+            // all higher indexes shift up
+            names.Insert(5, "Dave");
+
+            //remove an item by value
+            //higher indexes shift down
+            names.Remove("Jill");
+
+            //remove an item by index
+            //higher indexes shift down
+            names.RemoveAt(5);
+
+            //sort by value
+            names.Sort();
+
+            // iterate through the whole list
+            foreach (string name in names)
+            {
+                Console.WriteLine($"{name} is at index: {names.IndexOf(name)}");
+            }
+
             Console.Read();
         }
     }
