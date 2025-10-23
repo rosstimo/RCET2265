9e9245947e49ed1700a713d3eb73a4bd806337e5
rosstimo <rosstimo@isu.edu>
2025-10-23 08:58:26 -0600

    add some line builder, listbox add examples with random numbers and loops

--- Examples/WinFormEvenMoreControls/WinFormEvenMoreControls/MoreControlsForm.cs
+++ Examples/WinFormEvenMoreControls/WinFormEvenMoreControls/MoreControlsForm.cs
@@ -7,6 +7,55 @@ namespace WinFormEvenMoreControls
             InitializeComponent();
         }
 
+        //Program Logic -------------------------------------------------------
+
+        string BuildString()
+        {
+            string message = "";
+
+            for (int i = 0; i<=10; i++)
+            {
+                message += "test";
+            }
+            return message;
+        }
+
+        int RandomNumberInRange(int min = 0, int max = 10)
+        {
+            Random randomNumberInator = new Random();
+
+            return randomNumberInator.Next(min, max + 1);
+
+        }
+        void TestRandom()
+        {
+            int[] count = new int[11];
+            string line = "";
+
+            for (int i = 0;i<=10000;i++)
+            {
+                count[RandomNumberInRange()] += 1;
+            }
+            
+            foreach (int thingy in count)
+            {
+               line += thingy + " | ";
+            }
+
+            DisplayListBox.Items.Add(line);
+
+        }
+
+
+        int NumberAround(int myNumber)
+        {
+            RandomNumberInRange();
+            return 10;
+        }
+
+
+        //Event Handlers ------------------------------------------------------
+
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -14,12 +63,13 @@ namespace WinFormEvenMoreControls
 
         private void ClearButton_Click(object sender, EventArgs e)
         {
-            this.Text = "Clear";
+            DisplayListBox.Items.Clear();
         }
 
         private void SubmitButton_Click(object sender, EventArgs e)
         {
-            this.Text = "Submit";
+            //DisplayListBox.Items.Add(BuildString());
+           TestRandom();
         }
     }
 }
