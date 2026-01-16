a78097a6a39c4207a31e8618d5467df429058766
rosstimo <rosstimo@isu.edu>
2026-01-16 08:52:24 -0800

    add beep example

--- Examples/ConsoleExample/ConsoleExample/Program.cs
+++ Examples/ConsoleExample/ConsoleExample/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ConsoleExample
+﻿// Tim Rossiter
+// Spring 2026
+// RCET2265
+// Console Practice Program
+// github url
+
+namespace ConsoleExample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Console.Beep();
+            Console.Beep(100, 1000);
         }
     }
 }
