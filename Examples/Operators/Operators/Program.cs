735bb21e1967a5d7aa03b6f433711a4044dda24d
rosstimo <rosstimo@isu.edu>
2026-01-20 08:41:06 -0800

    add compound examples

--- Examples/Operators/Operators/Program.cs
+++ Examples/Operators/Operators/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-
+            /*
             Console.WriteLine(5 + 5);
             Console.WriteLine(5 - 3);
             Console.WriteLine(5 * 5);
@@ -14,6 +14,25 @@
             Console.WriteLine(13.0 / 5); // floating point division
             Console.WriteLine(13 / 5.0); // floating point division
             Console.WriteLine(13.0 / 5.0); // floating point division
+            */
+
+            //Console.WriteLine(5 > 2);
+            //Console.WriteLine(5 < 2);
+            //Console.WriteLine(5 == 2); // test if equal
+            //Console.WriteLine(5 != 2); // test if not equal
+            //Console.WriteLine(5 >= 2);
+            //Console.WriteLine(5 <= 2);
+
+            // compund operators
+            int a = 5; //assign 5 to variable a
+            //a = a + 9;
+            a += 9; //compound asignment of a + 9
+            Console.WriteLine(a);
+
+            Console.WriteLine(++a); // incriment then print
+            Console.WriteLine(a);
+            Console.WriteLine(a++); // print then increment
+            Console.WriteLine(a);
 
             //pause
             Console.Read();
