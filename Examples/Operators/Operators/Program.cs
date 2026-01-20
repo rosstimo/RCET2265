30b7767db16ec19ebe2b4583e7db30506db9682b
rosstimo <rosstimo@isu.edu>
2026-01-20 08:17:38 -0800

    add simple arithmatic operatoe examples

--- Examples/Operators/Operators/Program.cs
+++ Examples/Operators/Operators/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(5 + 5);
+            Console.WriteLine(5 - 3);
+            Console.WriteLine(5 * 5);
+            Console.WriteLine(13 / 5); // integer division
+            Console.WriteLine(13 % 5); // remainder
+            Console.WriteLine(13d / 5d); // floating point division
+            Console.WriteLine(13.0 / 5); // floating point division
+            Console.WriteLine(13 / 5.0); // floating point division
+            Console.WriteLine(13.0 / 5.0); // floating point division
+
+            //pause
+            Console.Read();
         }
     }
 }
