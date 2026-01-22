5f790c5379faa2231997d2249eaaab24a30a4654
rosstimo <rosstimo@isu.edu>
2026-01-22 08:47:35 -0800

    switch example with user input

--- Examples/ControlFlow/ControlFlow/Program.cs
+++ Examples/ControlFlow/ControlFlow/Program.cs
@@ -5,26 +5,48 @@
         static void Main(string[] args)
         {
 
-            int score = 101;
-            if (score >= 100)
+            //int score = 101;
+            //if (score >= 100)
+            //{
+            //    Console.WriteLine("Ace!");
+            //}
+
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("Excellent! You got an A.");
+            //}
+            //else if (score >= 70)
+            //{
+            //    Console.WriteLine("Good job! You passed.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Keep trying! Practice makes perfect.");
+            //}
+
+            Console.WriteLine("Please enter a number for the day of the week 1-7");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
             {
-                Console.WriteLine("Ace!");
+                case "1":
+                    Console.WriteLine("It's Monday. Time to start the week!");
+                    break;
+                case "2":
+                    Console.WriteLine("It's Tuesday. Keep going!");
+                    break;
+                case "3":
+                    Console.WriteLine("It's Wednesday. Halfway there!");
+                    break;
+                default:
+                    Console.WriteLine("It's another day. Make it count!");
+                    break;
             }
 
-            if (score >= 90)
-            {
-                Console.WriteLine("Excellent! You got an A.");
-            }
-            else if (score >= 70)
-            {
-                Console.WriteLine("Good job! You passed.");
-            }
-            else
-            {
-                Console.WriteLine("Keep trying! Practice makes perfect.");
-            }
-                //pause
-                Console.Read();
+
+
+            //pause
+            Console.Read();
         }
     }
 }
