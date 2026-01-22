5bf84e5223c894a3b1ff136dc191fe0996985f48
rosstimo <rosstimo@isu.edu>
2026-01-22 08:25:24 -0800

    if else

--- Examples/ControlFlow/ControlFlow/Program.cs
+++ Examples/ControlFlow/ControlFlow/Program.cs
@@ -5,10 +5,14 @@
         static void Main(string[] args)
         {
 
-            int score = 60;
-            if (score >= 90)
+            int score = 59;
+            if (score >= 60)
             {
-                Console.WriteLine("Excellent! You got an A.");
+                Console.WriteLine("Pass. You did well enough.");
+            }
+            else
+            {
+                Console.WriteLine("Fail. Let's review and try again.");
             }
 
             //pause
