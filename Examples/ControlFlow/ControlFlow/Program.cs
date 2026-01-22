59a500f221224c2069f5150ccc12b0005f1d7b01
rosstimo <rosstimo@isu.edu>
2026-01-22 08:21:37 -0800

    add simple iff statement

--- Examples/ControlFlow/ControlFlow/Program.cs
+++ Examples/ControlFlow/ControlFlow/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int score = 60;
+            if (score >= 90)
+            {
+                Console.WriteLine("Excellent! You got an A.");
+            }
+
+            //pause
+            Console.Read();
         }
     }
 }
