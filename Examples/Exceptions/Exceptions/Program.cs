0dd7a448a59acc4ddace7eeaa45b1635fc967d1c
rosstimo <rosstimo@isu.edu>
2026-01-26 08:53:49 -0800

    loop intil valit or quit

--- Examples/Exceptions/Exceptions/Program.cs
+++ Examples/Exceptions/Exceptions/Program.cs
@@ -6,18 +6,43 @@
         {
             string userInput = "";
             int result = 0;
-            
-            Console.WriteLine("Please Enter a Number: ");
-            userInput = Console.ReadLine();
-            Console.WriteLine($"You entered {userInput}");
-            try
+            bool isValid = false;
+            string message = "";
+
+            do
             {
-                result = int.Parse(userInput);
+                Console.WriteLine("Please Enter a Number: ");
+                Console.WriteLine("Enter \"Q\" to quit.");
+                userInput = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"You entered {userInput}");
+
+                try
+                {
+                    result = int.Parse(userInput);
+                    message = $"Good job! {result} is a number!";
+                    isValid = true;
+                }
+                catch (Exception ex)
+                {
+                    if (userInput == "Q" || userInput == "q")
+                    {
+                        message = "Have a nice day!";
+                        isValid = true;
+                    }
+                    else
+                    {
+                        message = $"{userInput} is not a number";
+                    }
+                }
+
+                Console.WriteLine(message);
+
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Something went wrong: " + ex.StackTrace);
-            }
+            while (!isValid);
+
+
+
             //pause
             Console.Read();
         }
