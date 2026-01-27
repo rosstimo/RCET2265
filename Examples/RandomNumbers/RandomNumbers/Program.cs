15de7fc1ad407a8755cbd219bd8219de34100457
rosstimo <rosstimo@isu.edu>
2026-01-27 08:50:33 -0800

    random number example

--- Examples/RandomNumbers/RandomNumbers/Program.cs
+++ Examples/RandomNumbers/RandomNumbers/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Random rand = new Random(); // constructor
+
+            //test randomness and range with a for loop
+            for (int i = 0; i < 1000; i++)
+            {
+                //rand.Next(min,max) max is not inclusive
+                int n = rand.Next(1, 7); // Random number between 1 and 6
+                
+                Console.Write(n);
+            }
+
+            //pause
+            Console.Read();
         }
     }
 }
