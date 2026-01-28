83da54c36c42d5dc1da4e9d959747fe7eb72daca
rosstimo <rosstimo@isu.edu>
2026-01-28 08:15:22 -0800

    init

---
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ArrayExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int thing = 0;
+            string[] fruits = new string[7]; 
+
+            //pause
+            Console.Read();
+        }
+    }
+}
