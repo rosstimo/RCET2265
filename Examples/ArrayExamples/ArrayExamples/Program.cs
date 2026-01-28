efc95cc0cce4a1e22aab7e3f708286e5fd73a650
rosstimo <rosstimo@isu.edu>
2026-01-28 08:23:45 -0800

    add one dimensional array example

--- Examples/ArrayExamples/ArrayExamples/Program.cs
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -5,7 +5,20 @@
         static void Main(string[] args)
         {
             int thing = 0;
-            string[] fruits = new string[7]; 
+            string[] fruits = new string[7];
+
+            fruits[0] = "apple";
+            fruits[1] = "banana";
+            fruits[2] = "kiwi";
+            fruits[3] = "tomato";
+            fruits[4] = "pineapple";
+            fruits[5] = "dragon fruit";
+            fruits[6] = "watermelon";
+            //fruits[7] = "tangerine"; //this will crash. index out of range
+
+
+            Console.WriteLine(fruits[3]);
+
 
             //pause
             Console.Read();
