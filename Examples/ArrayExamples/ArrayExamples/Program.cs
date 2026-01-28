f30347576c55193913ad37e6573d0437b67d6274
rosstimo <rosstimo@isu.edu>
2026-01-28 08:34:25 -0800

    add looping over array examples

--- Examples/ArrayExamples/ArrayExamples/Program.cs
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -15,9 +15,21 @@
             fruits[5] = "dragon fruit";
             fruits[6] = "watermelon";
             //fruits[7] = "tangerine"; //this will crash. index out of range
+            //Console.WriteLine(fruits[3]);
 
+            // read all the fruits
+            for (int i = 0; i < 7; i++) 
+            {
+                Console.WriteLine(fruits[i]);
+            }
 
-            Console.WriteLine(fruits[3]);
+            // same with no index or need for known length
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine($"The length of fruits is: {fruits.Length}");
 
 
             //pause
