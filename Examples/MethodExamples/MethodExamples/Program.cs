11becb6915f852a22004e17df1766cc371f08e35
rosstimo <rosstimo@isu.edu>
2026-01-29 08:24:08 -0800

    init

---
+++ Examples/MethodExamples/MethodExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MethodExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //pause
+            Console.Read();
+        }
+    }
+}
