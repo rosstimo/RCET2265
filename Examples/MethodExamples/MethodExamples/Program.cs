2aa1524c2ab12cc27823d2add9ece84df91147dc
rosstimo <rosstimo@isu.edu>
2026-01-29 08:42:11 -0800

    add simple method examples

--- Examples/MethodExamples/MethodExamples/Program.cs
+++ Examples/MethodExamples/MethodExamples/Program.cs
@@ -1,12 +1,39 @@
-﻿namespace MethodExamples
+﻿using System.Net.NetworkInformation;
+
+namespace MethodExamples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 5;
+
+            //SayHello();
+            //AddNumbers();
+            //AddNumbers();
+            //SayHello();
+            ShowCount();
+            Console.WriteLine(count);
             //pause
             Console.Read();
         }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void AddNumbers()
+        {
+           
+            Console.WriteLine(1 + 1);
+        }
+
+        static void ShowCount()
+        {
+            int count = 7;
+            Console.WriteLine(count);
+        }
+
     }
 }
