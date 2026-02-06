68762859a257a2f370c3b73022c43fe73fc4139d
rosstimo <rosstimo@isu.edu>
2026-02-06 08:54:28 -0700

    working inclusive random number method

--- Examples/BingoGame/BingoGame/Program.cs
+++ Examples/BingoGame/BingoGame/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * TODO:
  * [ ] Track bingo balls drawn in a two dimensional array
- * [ ] Display status of all balls on the console
+ * [x] Display status of all balls on the console
  * [ ] Clear all drawn balls to start a new game
  * [ ] Let the user quit
  * [ ] Draw a random ball
@@ -30,7 +30,9 @@ namespace BingoGame
             drawnBalls[2,13] = true;
             drawnBalls[0,11] = true;
 
-            Display();
+            // Display();
+
+            DrawBall();
             //pause
             Console.Read();
         }
@@ -71,5 +73,25 @@ namespace BingoGame
                 currentRow = ""; //reset 
             }
         }
+
+        static void DrawBall()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(RandomNumberZeroTo(14));
+            }
+        }
+/// <summary>
+/// Get a random integer from 0 to max inclusive.
+/// </summary>
+/// <param name="max"></param>
+/// <returns>integer</returns>
+        static private int RandomNumberZeroTo(int max)
+        {
+            int range = max + 1; //make max inclusive
+            Random rand = new Random();
+            return rand.Next(range);
+        }
+
     }
 }
