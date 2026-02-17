3d0f3443ae2d1ea5aae2947dd7fec62cce595498
rosstimo <rosstimo@isu.edu>
2026-02-17 08:10:05 -0800

    init

---
+++ FileIOExample/FileIOExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FileIOExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //pause
+            Console.Read();
+        }
+    }
+}
