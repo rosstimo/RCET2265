c74c1e970ac1fee6902599d513d0faea1caaec3d
rosstimo <rosstimo@isu.edu>
2026-04-02 08:27:29 -0600

    rename picturebox and update Form Text

--- Examples/GraphicExample/GraphicExample/GraphicsForm.Designer.cs
+++ Examples/GraphicExample/GraphicExample/GraphicsForm.Designer.cs
@@ -28,38 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            DisplayPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)DisplayPictureBox).BeginInit();
             SuspendLayout();
             // 
-            // pictureBox1
+            // DisplayPictureBox
             // 
-            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            pictureBox1.BackColor = SystemColors.ActiveCaption;
-            pictureBox1.Location = new Point(20, 60);
-            pictureBox1.Margin = new Padding(5, 6, 5, 6);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(971, 350);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            DisplayPictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DisplayPictureBox.BackColor = SystemColors.ActiveCaption;
+            DisplayPictureBox.Location = new Point(20, 60);
+            DisplayPictureBox.Margin = new Padding(5, 6, 5, 6);
+            DisplayPictureBox.Name = "DisplayPictureBox";
+            DisplayPictureBox.Size = new Size(971, 350);
+            DisplayPictureBox.TabIndex = 0;
+            DisplayPictureBox.TabStop = false;
             // 
             // GraphicsForm
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1016, 496);
-            Controls.Add(pictureBox1);
+            Controls.Add(DisplayPictureBox);
             Margin = new Padding(5, 6, 5, 6);
             MinimumSize = new Size(400, 400);
             Name = "GraphicsForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            Text = "Graphics Example";
+            ((System.ComponentModel.ISupportInitialize)DisplayPictureBox).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
+        private PictureBox DisplayPictureBox;
     }
 }
