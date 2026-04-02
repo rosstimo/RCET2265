0e8b3822d36e7646fb0ae7b4bf9a963bbb3f8ee3
rosstimo <rosstimo@isu.edu>
2026-04-02 08:45:35 -0600

    add draw line example

--- Examples/GraphicExample/GraphicExample/GraphicsForm.cs
+++ Examples/GraphicExample/GraphicExample/GraphicsForm.cs
@@ -6,7 +6,26 @@ namespace GraphicExample
         {
             InitializeComponent();
         }
+        
+        void DrawLine()
+        {
+            //create a Graphics object named g that draws on the picture box
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            // create a pen to draw with
+            Pen thePen = new Pen(Color.Black);
+            thePen.Width = 10;
+            //draw the line here
+            g.DrawLine(thePen,0,0,DisplayPictureBox.Width,DisplayPictureBox.Height);
 
+            //free up resources
+            g.Dispose();
+            thePen.Dispose();
+
+        }
+
+
+
+//Event Handlers --------------------------------------------------------------
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -14,7 +33,7 @@ namespace GraphicExample
 
         private void DrawButton_Click(object sender, EventArgs e)
         {
-            //draw
+            DrawLine();
         }
     }
 }
