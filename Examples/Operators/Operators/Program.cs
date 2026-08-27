6973e5d10c2bb9355556c861ff804e3ae8c146ce
rosstimo <rosstimo@isu.edu>
2026-08-27 08:49:13 -0600

    add some examples

--- Examples/Operators/Operators/Program.cs
+++ Examples/Operators/Operators/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Operators
+﻿//Tim Rossiter
+//RCET3371
+//Operator Example
+//https://github.com/rosstimo/RCET2265.git
+namespace Operators
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Operators
+            Console.WriteLine(2 + 2);
+            Console.WriteLine(5 - 3);
+            Console.WriteLine(5 * 2);
+            Console.WriteLine(15 / 3);
+            Console.WriteLine(15 / 4);
+            Console.WriteLine(15 % 4);
+            Console.ReadLine(); //pause before closing 
         }
     }
 }
