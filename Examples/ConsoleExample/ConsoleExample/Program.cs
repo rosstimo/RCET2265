0a14395e8b6fc80ca98fc853f02297452f68f425
rosstimo <rosstimo@isu.edu>
2026-08-31 08:10:34 -0600

    start console example W02D01

---
+++ Examples/ConsoleExample/ConsoleExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //send a message to  user
+            Console.WriteLine();
+            //get user input
+            Console.ReadLine();
+            //pause before close
+            Console.ReadLine();
+        }
+    }
+}
