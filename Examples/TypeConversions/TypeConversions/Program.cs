53723d37c385993bdccfc9165d69bad599491b47
rosstimo <rosstimo@isu.edu>
2026-09-01 08:24:13 -0600

    init

---
+++ Examples/TypeConversions/TypeConversions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypeConversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
