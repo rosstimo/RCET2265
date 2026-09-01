d67120a3abb3ddfd1a98b0886456c2dd3b7aae6e
rosstimo <rosstimo@isu.edu>
2026-09-01 08:50:46 -0600

    add capture user input and convert to integer to do math

--- Examples/TypeConversions/TypeConversions/Program.cs
+++ Examples/TypeConversions/TypeConversions/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = "";
+            int firstNumber;
+            int secondNumber;
+            int result = 0;
+
+            Console.WriteLine("Please enter a whole number:");
+            userInput = Console.ReadLine();
+            Console.WriteLine($"You entered {userInput}");
+            firstNumber = int.Parse(userInput);
+
+            Console.WriteLine("Please enter a whole number:");
+            userInput = Console.ReadLine();
+            Console.WriteLine($"You entered {userInput}");
+            secondNumber = int.Parse(userInput);
+
+            result = firstNumber + secondNumber;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+
+            //pause
+            Console.ReadLine();
         }
     }
 }
