8d98f809a5072f16231b4216066479075cc4e18c
rosstimo <rosstimo@isu.edu>
2026-09-03 08:28:37 -0600

    add if else if else example

--- Examples/ControlFlow/ControlFlow/Program.cs
+++ Examples/ControlFlow/ControlFlow/Program.cs
@@ -5,14 +5,28 @@
         static void Main(string[] args)
         {
             int firstNumber = 7;
+            //if (firstNumber > 1)
+            //{
+            //    Console.WriteLine("Bigger than 1!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not bigger than 1!");
+            //}
+
             if (firstNumber > 1)
             {
                 Console.WriteLine("Bigger than 1!");
             }
-            else
+            else if (firstNumber < 1)
             {
                 Console.WriteLine("Not bigger than 1!");
             }
+            else
+            {
+                Console.WriteLine("something else happened");
+            }
+
 
 
             //pause
