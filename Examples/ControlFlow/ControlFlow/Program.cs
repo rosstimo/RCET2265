e59450109ee50bfa8cb607b4d83f67b5b003bab0
rosstimo <rosstimo@isu.edu>
2026-09-03 08:37:55 -0600

    add simple user choice and evaluate

--- Examples/ControlFlow/ControlFlow/Program.cs
+++ Examples/ControlFlow/ControlFlow/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             int firstNumber = 7;
+            string userInput = "";
+
             //if (firstNumber > 1)
             //{
             //    Console.WriteLine("Bigger than 1!");
@@ -14,17 +16,37 @@
             //    Console.WriteLine("Not bigger than 1!");
             //}
 
-            if (firstNumber > 1)
+            //if (firstNumber > 1)
+            //{
+            //    Console.WriteLine("Bigger than 1!");
+            //}
+            //else if (firstNumber < 1)
+            //{
+            //    Console.WriteLine("Not bigger than 1!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("something else happened");
+            //}
+
+            Console.WriteLine("Choose wisely 1, 2, or 3...");
+            userInput = Console.ReadLine();
+
+            if (userInput == "1")
             {
-                Console.WriteLine("Bigger than 1!");
+                Console.WriteLine("you have chosen poorly");
             }
-            else if (firstNumber < 1)
+            else if (userInput == "2")
             {
-                Console.WriteLine("Not bigger than 1!");
+                Console.WriteLine("you have chosen poorly");
+            }
+            else if (userInput == "3")
+            {
+                Console.WriteLine("you have chosen wisely");
             }
             else
             {
-                Console.WriteLine("something else happened");
+                Console.WriteLine($"{userInput}!?... that is way off...");
             }
 
 
