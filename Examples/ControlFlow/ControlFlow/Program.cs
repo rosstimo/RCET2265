2be1befec738c5d3d8705b63dc92e976b67eb903
rosstimo <rosstimo@isu.edu>
2026-09-03 08:13:04 -0600

    start control flow example

---
+++ Examples/ControlFlow/ControlFlow/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 7;
+
+            Console.WriteLine(firstNumber > 1);
+
+        //pause
+            Console.ReadLine();
+        }
+    }
+}
