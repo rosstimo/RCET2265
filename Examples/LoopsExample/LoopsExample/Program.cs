b7b17fcaa9719557a153198113856baa3435d83b
rosstimo <rosstimo@isu.edu>
2026-09-04 08:53:12 -0600

    trap user until they enter a whole number

--- Examples/LoopsExample/LoopsExample/Program.cs
+++ Examples/LoopsExample/LoopsExample/Program.cs
@@ -16,23 +16,49 @@
             //    someNumber++;
             //}
 
-            string userInput = "Q";
-            while (userInput != "Q")
-            {
-                Console.WriteLine("Enter Q to quit:");
-                userInput = Console.ReadLine();
-                Console.Clear();
-                Console.WriteLine($"You entered {userInput}");
-            }
+            //string userInput = "Q";
+            //while (userInput != "Q")
+            //{
+            //    Console.WriteLine("Enter Q to quit:");
+            //    userInput = Console.ReadLine();
+            //    Console.Clear();
+            //    Console.WriteLine($"You entered {userInput}");
+            //}
 
+            //do
+            //{
+            //    Console.WriteLine("the do loop always runs at least once");
+            //    Console.WriteLine("Enter Q to quit:");
+            //    userInput = Console.ReadLine();
+            //    Console.Clear();
+            //    Console.WriteLine($"You entered {userInput}");
+            //} while (userInput != "Q");
+
+
+            string userInput = "";
+            int firstNumber = 0;
+            bool isValid = false;
             do
             {
-                Console.WriteLine("the do loop always runs at least once");
+                Console.WriteLine("Please enter a whole number:");
                 Console.WriteLine("Enter Q to quit:");
                 userInput = Console.ReadLine();
-                Console.Clear();
                 Console.WriteLine($"You entered {userInput}");
-            } while (userInput != "Q");
+                Console.Clear();
+                try
+                {
+                    firstNumber = int.Parse(userInput);
+                    isValid = true;
+                    Console.WriteLine("Success!");
+                }
+                catch (Exception)
+                {   
+                    isValid = false;
+                    Console.WriteLine("FAIL!!!");
+                }
+            } while (!isValid);
+
+
 
             // Console.Clear();
             Console.WriteLine("Have a nice day!");
