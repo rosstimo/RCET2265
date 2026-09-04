c7a6e9109a27e9e8e1b6705a78a44821e3e275d9
rosstimo <rosstimo@isu.edu>
2026-09-04 08:28:55 -0600

    trap user until they quit

--- Examples/LoopsExample/LoopsExample/Program.cs
+++ Examples/LoopsExample/LoopsExample/Program.cs
@@ -9,13 +9,23 @@
             //    Console.WriteLine("hello");
             //}
 
-            int someNumber = 0;
-            while (someNumber <= 7)
-            {
-                Console.WriteLine("in the while loop");
-                someNumber++;
-            }
+            //int someNumber = 0;
+            //while (someNumber <= 7)
+            //{
+            //    Console.WriteLine("in the while loop");
+            //    someNumber++;
+            //}
 
+            string userInput = "";
+            while (userInput != "Q")
+            {
+                Console.WriteLine("Enter Q to quit:");
+                userInput = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"You entered {userInput}");
+            }
+            Console.Clear();
+            Console.WriteLine("Have a nice day!");
             //pause
             Console.ReadLine();
         }
