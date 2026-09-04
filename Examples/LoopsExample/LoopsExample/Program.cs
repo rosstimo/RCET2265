46a0879bdba28917c82913ffdaff27d397272620
rosstimo <rosstimo@isu.edu>
2026-09-04 08:20:04 -0600

    add while loop example

--- Examples/LoopsExample/LoopsExample/Program.cs
+++ Examples/LoopsExample/LoopsExample/Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("hello");
+            //}
+
+            int someNumber = 0;
+            while (someNumber <= 7)
             {
-                Console.WriteLine("hello");
+                Console.WriteLine("in the while loop");
+                someNumber++;
             }
+
             //pause
             Console.ReadLine();
         }
