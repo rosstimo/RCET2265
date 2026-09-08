b2f74a8cec32ad6d4b7c3b1347ee52f54a8dc2c2
rosstimo <rosstimo@isu.edu>
2026-09-08 08:13:08 -0600

    create array example

---
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
