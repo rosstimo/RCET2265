36491d928609ce063abf305ff87b047bf0fa5ddd
rosstimo <rosstimo@isu.edu>
2026-09-08 08:52:03 -0600

    add multidimensional array example

--- Examples/ArrayExamples/ArrayExamples/Program.cs
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             int[] measurements = new int[5];
+            int[,] grid = new int[3, 5];
 
             measurements[2] = 34;
             measurements[4] = 47;
@@ -17,7 +18,21 @@
 
            Console.WriteLine($"the array is {measurements.Length} elements long");
 
-        //pause
+
+            grid[0, 0] = 35;
+            grid[2, 3] = 55;
+            grid[2, 4] = 999;
+            foreach (int coordinate in grid)
+            {
+                Console.WriteLine(coordinate);
+            }
+            Console.WriteLine($"the array is {grid.Length} elements in length");
+            Console.WriteLine($"the array is {grid.LongLength} elements long length");
+
+
+
+
+            //pause
             Console.ReadLine();
         }
     }
