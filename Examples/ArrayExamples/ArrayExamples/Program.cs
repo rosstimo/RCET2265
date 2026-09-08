c2f4b652639428b963d0f1fb49a96150dc86d893
rosstimo <rosstimo@isu.edu>
2026-09-08 08:34:16 -0600

    loop through array and get length

--- Examples/ArrayExamples/ArrayExamples/Program.cs
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -9,8 +9,13 @@
             measurements[2] = 34;
             measurements[4] = 47;
             measurements[0] = 55;
+            
+            for (int i =0; i < 5; i++)
+            {
+                Console.WriteLine(measurements[i]);
+            }
 
-            Console.WriteLine(measurements[3]);
+           Console.WriteLine($"the array is {measurements.Length} elements long");
 
         //pause
             Console.ReadLine();
