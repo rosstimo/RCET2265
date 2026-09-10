67880443e15c8df0d1d39978194f9b89a25f5b87
rosstimo <rosstimo@isu.edu>
2026-09-10 08:39:04 -0600

    show iterate over array with nested loop for visual

--- Examples/ArrayExamples/ArrayExamples/Program.cs
+++ Examples/ArrayExamples/ArrayExamples/Program.cs
@@ -5,29 +5,21 @@
         static void Main(string[] args)
         {
             int[] measurements = new int[5];
-            int[,] grid = new int[3, 5];
 
-            measurements[2] = 34;
-            measurements[4] = 47;
-            measurements[0] = 55;
+            measurements[0] = 1;
+            measurements[1] = 7;
+            measurements[2] = 3;
+            measurements[3] = 5;
+            measurements[4] = 2;
             
             foreach (int measurement in measurements)
             {
-                Console.WriteLine(measurement);
+                for (int i = 0; i < measurement; i++)
+                {
+                    Console.Write("-");
+                }
+                Console.WriteLine(">");
             }
-
-           Console.WriteLine($"the array is {measurements.Length} elements long");
-
-
-            grid[0, 0] = 35;
-            grid[2, 3] = 55;
-            grid[2, 4] = 999;
-            foreach (int coordinate in grid)
-            {
-                Console.WriteLine(coordinate);
-            }
-            Console.WriteLine($"the array is {grid.Length} elements in length");
-            Console.WriteLine($"the array is {grid.LongLength} elements long length");
 
 
 
