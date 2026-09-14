c2d00111fce76f0fc72511bcbf24b2de0a3bc6e8
rosstimo <rosstimo@isu.edu>
2026-09-14 08:07:37 -0600

    create random numbers example

---
+++ Examples/RandomNumbersExample/RandomNumbersExample/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RandomNumbersExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Read();
+        }
+    }
+}
