ee904d1466c4045cfeb3fa8f005cdc18d2a5e5ab
rosstimo <rosstimo@isu.edu>
2026-09-15 08:29:03 -0600

    loop to further show re usability

--- Examples/MethodExamples/MethodExamples/Program.cs
+++ Examples/MethodExamples/MethodExamples/Program.cs
@@ -4,14 +4,18 @@
     {
         static void Main(string[] args)
         {
-            PrintGreeting("Nancy");
+            string[] names = { "Joe", "Nancy", "Jimmy", "Sally" , "Bob"};
+            foreach (string name in names )
+            {
+                PrintGreeting(name);
+            }
             // pause
             Console.ReadLine();
         }
 
         static void PrintGreeting(string name)
         {
-            Console.WriteLine($"hello, {name}");
+            Console.WriteLine($"Hello, {name}!");
         }
     }
 }
