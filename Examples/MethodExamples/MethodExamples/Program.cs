58120255d508c56c6114d2952e637458973e963d
rosstimo <rosstimo@isu.edu>
2026-09-15 08:10:42 -0600

    create method examples

---
+++ Examples/MethodExamples/MethodExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MethodExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // pause
+            Console.ReadLine();
+        }
+    }
+}
