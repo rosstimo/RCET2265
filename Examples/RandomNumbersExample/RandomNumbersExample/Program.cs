76af41b76d9323fffb26afe6d81bcdab4a2b32b8
rosstimo <rosstimo@isu.edu>
2026-09-17 08:54:22 -0600

    show sum of two random numbers distributions

--- Examples/RandomNumbersExample/RandomNumbersExample/Program.cs
+++ Examples/RandomNumbersExample/RandomNumbersExample/Program.cs
@@ -6,17 +6,17 @@
         {
             Random rnd = new();
             int choice = 0;
-            int[] rolls = new int[8];
+            int[] rolls = new int[14];
 
             for (int i = 0; i < 1000; i++)
             {
-                choice = rnd.Next(1,7);
+                choice = rnd.Next(1,7) + rnd.Next(1,7);
 
                 switch (choice) 
                 {
-                    //case 0:
-                    //    rolls[0]++;
-                    //    break;
+                    case 0:
+                        rolls[0]++;
+                        break;
                     case 1:
                         rolls[1]++;
                         break;
@@ -35,9 +35,27 @@
                     case 6:
                         rolls[6]++;
                         break;
-                    //case 7:
-                    //    rolls[7]++;
-                    //    break;
+                    case 7:
+                        rolls[7]++;
+                        break;
+                    case 8:
+                        rolls[8]++;
+                        break;
+                    case 9:
+                        rolls[9]++;
+                        break;
+                    case 10:
+                        rolls[10]++;
+                        break;
+                    case 11:
+                        rolls[11]++;
+                        break;
+                    case 12:
+                        rolls[12]++;
+                       break;
+                    case 13:
+                        rolls[13]++;
+                        break;
                     default:
                         Console.WriteLine($"You rolled a {choice}. So something weird happened.");
                         break;
@@ -46,7 +64,12 @@
 
             foreach (int rollcount in rolls)
             {
-                Console.WriteLine(rollcount);
+                //Console.WriteLine(rollcount);
+                for (int i = 0;i < rollcount;i += 10)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
             Console.WriteLine("All done");
             Console.Read();
