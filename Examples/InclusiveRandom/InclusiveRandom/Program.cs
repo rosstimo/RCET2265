df235dcf754b2ad5a5e06987b3a2ceffc83af9c0
rosstimo <rosstimo@isu.edu>
2026-09-18 08:26:38 -0600

    add TODOs

--- Examples/InclusiveRandom/InclusiveRandom/Program.cs
+++ Examples/InclusiveRandom/InclusiveRandom/Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-
+        
             // pause
             Console.ReadLine();
         }
+        
+        // TODO
+        // [ ] Random number from min to max inclusive
+        // [ ] returns and integer
+        // [ ] takes two argument max, min in that order
+        // [ ] later make max and min optional with reasonable default values
     }
 }
