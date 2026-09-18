594352ae8c0e89f41b0fc754faa3684c2e22e8bb
rosstimo <rosstimo@isu.edu>
2026-09-18 08:31:23 -0600

    start method

--- Examples/InclusiveRandom/InclusiveRandom/Program.cs
+++ Examples/InclusiveRandom/InclusiveRandom/Program.cs
@@ -12,8 +12,13 @@
         // TODO
         // [ ] Random number from min to max inclusive
         // [ ] returns and integer
-        // [ ] takes two argument max, min in that order
+        // [x] takes two argument max, min in that order
         // [ ] later make max and min optional with reasonable default values
+
+        static void RandomNumberFrom(int max, int min)
+        {
+
+        }
 
 
     }
