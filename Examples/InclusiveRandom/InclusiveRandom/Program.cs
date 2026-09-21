5181412417641e77dc6f9af362f88a61358669f3
rosstimo <rosstimo@isu.edu>
2026-09-21 08:45:13 -0600

    working test

--- Examples/InclusiveRandom/InclusiveRandom/Program.cs
+++ Examples/InclusiveRandom/InclusiveRandom/Program.cs
@@ -11,22 +11,25 @@
 
         static void TestRandomNumberInclusive()
         {
-            for (int i = 0; i < 1000; i++)
+            int currentNumber; 
+            for (int i = 0; i < 1000000; i++)
             {
-                if (RandomNumberFrom(10, 3) < 3 || RandomNumberFrom(10, 3) > 10)
+                currentNumber = RandomNumberFrom(10, 3);
+                if (currentNumber < 3 || currentNumber > 10)
                 {
-                    Console.WriteLine($"Error {RandomNumberFrom(10, 3) } is out of range");
+                    Console.WriteLine($"Error {currentNumber} is out of range");
                 }
                 //else
                 //{ 
-                //    Console.WriteLine(RandomNumberFrom(10, 3));
+                //    Console.WriteLine(currentNumber(10, 3));
                 //}
             }
+                Console.WriteLine("Test Complete");
 
         }
         
         // TODO
-        // [ ] Random number from min to max inclusive
+        // [x] Random number from min to max inclusive
         // [x] returns and integer
         // [x] takes two argument max, min in that order
         // [ ] later make max and min optional with reasonable default values
@@ -34,7 +37,7 @@
         static int RandomNumberFrom(int max, int min)
         {
             Random randy = new Random();
-            return randy.Next(min, max + 1 + 1);
+            return randy.Next(min, max + 1);
         }
 
 
