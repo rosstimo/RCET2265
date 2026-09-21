3e7684ef43ed9acea8a4d4798f3ea9e8ef861c2f
rosstimo <rosstimo@isu.edu>
2026-09-21 08:52:04 -0600

    optional args and dynamic test

--- Examples/InclusiveRandom/InclusiveRandom/Program.cs
+++ Examples/InclusiveRandom/InclusiveRandom/Program.cs
@@ -11,20 +11,22 @@
 
         static void TestRandomNumberInclusive()
         {
+            int max = 1;
+            int min = 0;
             int currentNumber; 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 1000; i++)
             {
-                currentNumber = RandomNumberFrom(10, 3);
-                if (currentNumber < 3 || currentNumber > 10)
+                currentNumber = RandomNumberFrom(max,min);
+                if (currentNumber < min || currentNumber > max)
                 {
                     Console.WriteLine($"Error {currentNumber} is out of range");
                 }
-                //else
-                //{ 
-                //    Console.WriteLine(currentNumber(10, 3));
-                //}
+                else
+                {
+                    Console.WriteLine(currentNumber);
+                }
             }
-                Console.WriteLine("Test Complete");
+            Console.WriteLine("Test Complete");
 
         }
         
@@ -32,9 +34,9 @@
         // [x] Random number from min to max inclusive
         // [x] returns and integer
         // [x] takes two argument max, min in that order
-        // [ ] later make max and min optional with reasonable default values
+        // [x] later make max and min optional with reasonable default values
 
-        static int RandomNumberFrom(int max, int min)
+        static int RandomNumberFrom(int max = 10, int min = 1)
         {
             Random randy = new Random();
             return randy.Next(min, max + 1);
