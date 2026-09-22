8c603864e450b901063b6aa896e943f4ae8670de
rosstimo <rosstimo@isu.edu>
2026-09-22 08:39:49 -0600

    add new line

--- Examples/AccumulateMessages/AccumulateMessages/Program.cs
+++ Examples/AccumulateMessages/AccumulateMessages/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace AccumulateMessages
+using System;
+
+public static class Program
 {
-    internal class Program
+    private static string messages = "";
+
+    // Do not change the code in Main
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string input = "";
+        Console.WriteLine("Choose test mode: (M)anual, (A)uto, or (Q)uit");
+        input = Console.ReadLine();
+        
+        switch (input.Trim().ToUpper())
         {
-            Console.WriteLine("Hello, World!");
+            case "M":
+                Test.Manual();
+                break;
+            case "A":
+                Test.Auto();
+                break;
+            case "Q":
+                Console.WriteLine("Quitting.");
+                break;
+            default:
+                Console.WriteLine("Invalid input. Quitting.");
+                break;
         }
+        // pause
+        Console.ReadLine();
     }
+        //TODO:
+        // [ ] - If clear is true, erase all saved messages.
+        // [ ] - Do not save empty messages.
+        // [x] - add a new line after each new message.
+        // [x] - Always return currently saved messages.
+        // [ ] - Make sure all auto tests pass.
+    public static string UserMessages(string newMessage, bool clear)
+    {
+        //Write your code here.
+        messages += newMessage + "\n";
+        return messages;
+    }
+
+
 }
